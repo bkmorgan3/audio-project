0200277b4b291095fbb7be05c40365067b225264
Brad Morgan <bkmorgan3@gmail.com>
2025-10-01 18:16:27 -0700

    render audio player

--- src/app/(audio)/transcribe/page.tsx
+++ src/app/(audio)/transcribe/page.tsx
@@ -1,9 +1,10 @@
+import AudioPlayer from "@/app/components/audioPlayer"
+
 export default async function TranscribePage() {
     return <div className="container mx-auto px-4 py-12 text-yellow ">
-        <h1>Transcribe page</h1>
-        <div className="border border-red-400">
-            <label htmlFor="audio-file-input">Choose an audio file:</label> <br />
-            <input type="file" id="audio-file-input" accept="audio/*"></input>
+        <h1>Hello, Please upload a file to get started</h1>
+        <div>
+            <AudioPlayer />
         </div>
     </div>
 }
