a63fb14f15c203b06aaf0f46f8c6d8e2d6c03529
Brad Morgan <bkmorgan3@gmail.com>
2025-11-01 22:38:37 -0700

    add visualization, starting point

--- src/app/components/audioPlayer.tsx
+++ src/app/components/audioPlayer.tsx
@@ -9,6 +9,7 @@ export const AudioPlayer = () => {
     const [isPlaying, setIsPlaying] = useState(false)
     const sourceRef = useRef<AudioBufferSourceNode | null>(null)
     const animationRef = useRef<number | null>(null)
+    const canvasRef = useRef<HTMLCanvasElement | null>(null)
 
     useEffect(() => {
         if (typeof window !== 'undefined') {
@@ -69,7 +70,40 @@ export const AudioPlayer = () => {
     }
 
     const drawVisualizer = () => {
-        console.log("drawing data here")
+      const canvas = canvasRef.current
+      const analyser = analyserRef.current
+      if(!canvas || !analyser) return
+
+      const ctx = canvas.getContext('2d')
+      const bufferLength = analyser.fftSize
+      const dataArray = new Uint8Array(bufferLength)
+
+      const draw = () => {
+        analyser.getByteTimeDomainData(dataArray)
+        ctx!.fillStyle = '#111'
+        ctx!.fillRect(0,0,canvas.width, canvas.height)
+
+        ctx!.lineWidth = 2
+        ctx!.strokeStyle = '#00ff88'
+        ctx!.beginPath()
+        const sliceWidth = canvas.width / bufferLength
+        let x = 0
+
+        for (let i = 0; i < bufferLength; i++) {
+            const v = dataArray[i] / 128.0
+            const y = (v * canvas.height) / 2
+
+            if (i === 0) ctx!.moveTo(x,y)
+            else ctx!.lineTo(x,y)
+
+            x += sliceWidth
+        }
+        ctx!.lineTo(canvas.width, canvas.height / 2)
+        ctx!.stroke()
+
+        animationRef.current = requestAnimationFrame(draw)
+      }
+      draw()
     }
     return <section>
         <div className="border border-red-600 flex flex-col" >
@@ -77,9 +111,15 @@ export const AudioPlayer = () => {
             <input onChange={handleFile} className="hidden" type="file" id="file" accept="audio/*" />
             {audioBuffer && (
                 <button onClick={togglePlay} className="mt-2 border border-green-500 rounded px-4 py-1">
-                    {isPlaying ? "Stop iiiit" : "Play"}
+                    {isPlaying ? "Stop" : "Play"}
                 </button>
             )}
         </div>
+        <canvas 
+        ref={canvasRef}
+        width={600}
+        height={200}
+        className="border border-gray-700 rounded bg-black"
+        />
     </section>
 }
